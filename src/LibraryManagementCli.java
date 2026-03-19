9b1cfdedaaa49334853c0315347e8c62d2731359
Daniel Maina <kamau121593@students.must.ac.ke>
2026-03-19 21:25:20 +0300

    Added a new java file

--- src/LibraryManagementCli.java
+++ src/LibraryManagementCli.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class LibraryManagerCli {
+public class LibraryManagementCli {
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/contactdb"; // use your DB
         String user = "postgres";
