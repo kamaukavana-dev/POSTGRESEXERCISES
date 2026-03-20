b3f619df3b852a8a8df42cd4d59f9e1eb857af89
Daniel Maina <kamau121593@students.must.ac.ke>
2026-03-20 19:10:28 +0300

    modified this java file

--- src/StudentGradeCli.java
+++ src/StudentGradeCli.java
@@ -10,16 +10,7 @@ public class StudentGradesCLI {
         try (Connection conn = DriverManager.getConnection(url, user, password);
              Scanner scanner = new Scanner(System.in)) {
 
-            while (true) {
-                System.out.println("\n--- StudentGradesCLI ---");
-                System.out.println("1. Add Student");
-                System.out.println("2. Add Grade");
-                System.out.println("3. List All Grades");
-                System.out.println("4. Show Students Above Average");
-                System.out.println("5. Show Students With Failing Grades");
-                System.out.println("6. Exit");
-                System.out.print("Choose: ");
-                String choice = scanner.nextLine();
+
 
 
 
