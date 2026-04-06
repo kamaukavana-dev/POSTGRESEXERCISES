193434f786bdaab9068a358e204a4e58b3e0604c
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-06 20:46:00 +0300

    Modified this java file

--- src/FinalProjectCli.java
+++ src/FinalProjectCli.java
@@ -7,20 +7,7 @@ public class FinalProjectCLI {
         String user = "postgres";
         String password = "yourpassword";
 
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-             Scanner scanner = new Scanner(System.in)) {
 
-            conn.setAutoCommit(false); // transaction control
-
-            while (true) {
-                System.out.println("\n--- FinalProjectCLI ---");
-                System.out.println("1. Add Customer");
-                System.out.println("2. Add Product");
-                System.out.println("3. Place Order");
-                System.out.println("4. Show Sales Summary");
-                System.out.println("5. Exit");
-                System.out.print("Choose: ");
-                String choice = scanner.nextLine();
 
 
 
