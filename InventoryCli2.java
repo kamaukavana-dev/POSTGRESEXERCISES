ee7bb97e781513c6452f7ac6f8ba36d0a05f7a11
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-07 20:57:11 +0300

    Modified this java file

--- InventoryCli2.java
+++ InventoryCli2.java
@@ -7,18 +7,7 @@ public class InventoryCLI {
         String user = "postgres";
         String password = "yourpassword";
 
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-             Scanner scanner = new Scanner(System.in)) {
 
-            while (true) {
-                System.out.println("\n--- InventoryCLI ---");
-                System.out.println("1. Add Product");
-                System.out.println("2. Update Stock");
-                System.out.println("3. Show Inventory");
-                System.out.println("4. Show Restock Alerts");
-                System.out.println("5. Exit");
-                System.out.print("Choose: ");
-                String choice = scanner.nextLine();
 
 
 
