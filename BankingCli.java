44f4323b3ab4131be7191d45aeea81e66dd9b71a
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-12 22:31:13 +0300

    Modified this java file

--- BankingCli.java
+++ BankingCli.java
@@ -12,21 +12,7 @@ public class BankingCLI {
 
             conn.setAutoCommit(false); // transaction control
 
-            while (true) {
-                System.out.println("\n--- BankingCLI ---");
-                System.out.println("1. Create Account");
-                System.out.println("2. Show Accounts");
-                System.out.println("3. Transfer Money");
-                System.out.println("4. Show Transactions");
-                System.out.println("5. Exit");
-                System.out.print("Choose: ");
-                String choice = scanner.nextLine();
 
-                if (choice.equals("1")) {
-                    System.out.print("Owner name: ");
-                    String owner = scanner.nextLine();
-                    System.out.print("Initial balance: ");
-                    double balance = Double.parseDouble(scanner.nextLine());
 
 
 
