931e4ab8714ba42d5e5bf878dcb557c37d64d8af
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-13 20:54:46 +0300

    Modified this java file

--- SurveyCli.java
+++ SurveyCli.java
@@ -10,17 +10,7 @@ public class SurveyCLI {
         try (Connection conn = DriverManager.getConnection(url, user, password);
              Scanner scanner = new Scanner(System.in)) {
 
-            while (true) {
-                System.out.println("\n--- SurveyCLI ---");
-                System.out.println("1. Add Question");
-                System.out.println("2. Record Response");
-                System.out.println("3. Show Average Ratings");
-                System.out.println("4. Show Questions with >= 5 Responses");
-                System.out.println("5. Exit");
-                System.out.print("Choose: ");
-                String choice = scanner.nextLine();
 
-}
 
 
 
