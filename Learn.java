279c80321abef17fd5c9f0558d7a919eafb95963
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-16 16:55:49 +0300

    Modified this java file

--- Learn.java
+++ Learn.java
@@ -14,11 +14,4 @@ class Triangle{
         return Base * Height * 0.5;
     }
 }
-public class Learn {
-    public static void main(String[] args) {
-        Triangle t1 = new Triangle(10.5f, 5.0f);
-        Triangle t2 = new Triangle(8, 4);
-        System.out.println("Area of Triangle 1: " + t1.getArea()+" metres");
-        System.out.println("Area of Triangle 2: " + t2.getArea() + " metres");
-    }
-}
+
