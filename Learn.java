385fd369e767ce4d55afa0eb4a0dea74f68dd9c9
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-16 16:55:25 +0300

    Modified this java file

--- Learn.java
+++ Learn.java
@@ -1,24 +1,24 @@
 
-class BankAccount{
-    private String FirstName;
-    private String LastName;
-    private int AccountNumber;
-    private int Balance;
-
-    public BankAccount(String firstName, String lastName, int accountNumber, int balance) {
-        this.FirstName = firstName;
-        this.LastName = lastName;
-        this.AccountNumber = accountNumber;
-        this.Balance = balance;
+class Triangle{
+    private float Base;
+    private float Height;
+    Triangle(float Base,float height){
+        this.Base = Base;
+        this.Height = height;
     }
-    //setters
-
-
-
-
-
-
-
-
-
+    Triangle(int Base ,int height){
+        this.Base = Base;
+        this.Height = height;
+    }
+    public double getArea(){
+        return Base * Height * 0.5;
+    }
+}
+public class Learn {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle(10.5f, 5.0f);
+        Triangle t2 = new Triangle(8, 4);
+        System.out.println("Area of Triangle 1: " + t1.getArea()+" metres");
+        System.out.println("Area of Triangle 2: " + t2.getArea() + " metres");
+    }
 }
