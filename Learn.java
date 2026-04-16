b08b57420e2587a64333b1d485f17ab9b027a43f
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-16 17:23:28 +0300

    Modified this java file

--- Learn.java
+++ Learn.java
@@ -4,7 +4,5 @@ enum Calendar{
 }
 enum Days {
     Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
-}enum Months {
-    January, February, March, April, May, June, July, August, September, October, November, December;
 }
 
