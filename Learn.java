f84302ef1f338a963d765ef300733b8b77a2ae42
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-16 16:56:22 +0300

    Modified this java file

--- Learn.java
+++ Learn.java
@@ -2,14 +2,7 @@
 class Triangle{
     private float Base;
     private float Height;
-    Triangle(float Base,float height){
-        this.Base = Base;
-        this.Height = height;
-    }
-    Triangle(int Base ,int height){
-        this.Base = Base;
-        this.Height = height;
-    }
+
 
 }
 
