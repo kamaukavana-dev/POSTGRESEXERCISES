469b10ba14e89d02cd43892604a8f4429f0c1754
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-16 17:10:14 +0300

    Modified this java file

--- Learn.java
+++ Learn.java
@@ -2,9 +2,7 @@
 import java.util.Arrays;
 class arrays{
     private int [] myArr = {1,2,3};
-    arrays(int [] myArr){
-        this.myArr = Arrays.copyOf(myArr, myArr.length); //copy in
-    }
+
 
 }
 
