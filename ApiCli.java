9d207cfd2aa55b5927fbe9de17f3f5db8e418fb1
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-17 22:11:35 +0300

    Modified this java file

--- ApiCli.java
+++ ApiCli.java
@@ -7,18 +7,7 @@ public class ApiCli {
         String user = "postgres";
         String password = "yourpassword";
 
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-             Scanner scanner = new Scanner(System.in)) {
 
-            while (true) {
-                System.out.println("\n--- APICLI ---");
-                System.out.println("1. Register User");
-                System.out.println("2. Authenticate User");
-                System.out.println("3. Perform Action (CRUD)");
-                System.out.println("4. Show Logs");
-                System.out.println("5. Exit");
-                System.out.print("Choose: ");
-                String choice = scanner.nextLine();
 
 
 
