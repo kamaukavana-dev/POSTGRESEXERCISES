69d4732640601ac93fc29e3c54ccffa5b9b168df
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-19 20:02:58 +0300

    Modified this java file

--- ConstructorChaining.java
+++ ConstructorChaining.java
@@ -4,8 +4,23 @@ class Animal {
     }
 }
 
+class Dog extends Animal {
+    public Dog() {
+        super();// Automatically inserted by Java if omitted
+        System.out.println("Dog constructor");
+    }
+}
 
+class Puppy extends Dog {
+    public Puppy() {
+        super();
+        System.out.println("Puppy constructor");
+    }
+}
 
+public class ConstructorChaining {
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+    }
 
-
-
+}
