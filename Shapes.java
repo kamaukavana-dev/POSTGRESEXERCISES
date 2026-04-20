e6dc51546d306b643ca61dd98d906c066ec3f535
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-20 16:27:14 +0300

    Modified this java file

--- Shapes.java
+++ Shapes.java
@@ -18,15 +18,52 @@ class Shape {
         return "Shape{ color='" + color + "', area=" + getArea() + "}";
     }
 }
+class Circle extends Shape {
+    private double radius;
 
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
 
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
 
+    @Override
+    public String toString() {
+        return "Circle = [ color='" + getColor() + "', radius=" + radius + ", area=" + getArea() + " ]";
+    }
+}
+class Rectangle extends Shape {
+    private double width;
+    private double height;
 
+    public Rectangle(String color, double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
 
+    @Override
+    public double getArea() {
+        return width * height;
+    }
 
-
-
+    @Override
+    public String toString() {
+        return "Rectangle= [ color='" + getColor() + "', width=" + width
+                + ", height=" + height + ", area=" + getArea() + " ]";
+    }
 
 }
+public class Shapes {
+    public static void main(String[] args) {
+        Shape circle = new Circle("Red", 7.0);
+        Shape rectangle = new Rectangle("Blue", 4.0, 6.0);
 
+        System.out.println(circle);
+        System.out.println(rectangle);
+    }
 }
