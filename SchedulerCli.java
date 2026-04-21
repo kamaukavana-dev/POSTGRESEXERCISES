3e5de5f9752a83928fc43234ea8d8a111b5eca39
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-21 19:13:38 +0300

    Modified this java file

--- SchedulerCli.java
+++ SchedulerCli.java
@@ -7,18 +7,7 @@ public class SchedulerCLI {
         String user = "postgres";
         String password = "yourpassword";
 
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-             Scanner scanner = new Scanner(System.in)) {
-
-            while (true) {
-                System.out.println("\n--- SchedulerCLI ---");
-                System.out.println("1. Add Task");
-                System.out.println("2. Show Upcoming Tasks");
-                System.out.println("3. Show Overdue Tasks");
-                System.out.println("4. Mark Task Complete");
-                System.out.println("5. Exit");
-                System.out.print("Choose: ");
-                String choice = scanner.nextLine();
+);
 
 
 
